c6f3fe86ac0d600bc3a0299ae01eb1e803ee3bb0
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-11-18 20:10:29 +0000

    inserting data

--- SPRINT2/Whole Online shop/js/index.js
+++ SPRINT2/Whole Online shop/js/index.js
@@ -1,10 +1,18 @@
-let http = new XMLHttpRequest();
-http.open('get', './json/products.json',true);
-http.send();
-http.onload = function(){
-    if(this.readyState == 4 && this.status == 200){
-        let products = JSON.parse(this.responseText);
-        console.log(products)
-        console.log(products.camisetas)
+function loadData(){
+    let http = new XMLHttpRequest();
+    http.open('get', './json/products.json',true);
+    http.send();
+    http.onload = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let products = JSON.parse(this.responseText);
+            console.log(products[0])
+            //Load Categories.
+            for (let i = 1; i < 2; i++){
+                //document.getElementById('category'+i).innerHTML +=
+                //"<h6>"products[0]"</h6>"
+            }
+        }
     }
 }
+
+loadData()
