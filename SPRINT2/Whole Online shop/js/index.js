cf63591ad0a4713ce2826409b45c316f8332ebb0
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-11-18 20:32:52 +0000

    trying to insert html but fail

--- SPRINT2/Whole Online shop/js/index.js
+++ SPRINT2/Whole Online shop/js/index.js
@@ -5,11 +5,21 @@ function loadData(){
     http.onload = function(){
         if(this.readyState == 4 && this.status == 200){
             let products = JSON.parse(this.responseText);
-            console.log(products[0])
             //Load Categories.
             for (let i = 1; i < 2; i++){
-                //document.getElementById('category'+i).innerHTML +=
-                //"<h6>"products[0]"</h6>"
+                document.getElementById('category'+i).innerHTML +=`
+                <a class="text-decoration-none" href="">
+                    <div class="cat-item d-flex align-items-center mb-4">
+                        <div class="overflow-hidden" style="width: 100px; height: 100px;">
+                            <img class="img-fluid" src="img/cat-1.jpg" alt="">
+                        </div>
+                        <div class="flex-fill pl-3" id="category1">
+                            <h6>Category Name</h6>
+                            <small class="text-body">100 Products</small>
+                        </div>
+                    </div>
+                </a>
+                `
             }
         }
     }
