a69acc7cdf6ede4d1f833585550b647eea2c0eca
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-11-19 09:50:09 +0000

    Loading recent products in idex.html

--- SPRINT2/Whole Online shop/js/index.js
+++ SPRINT2/Whole Online shop/js/index.js
@@ -52,6 +52,38 @@ function loadData(){
                 </div>
                 `
             }
+
+            //Load Recent Products
+            
+            for (let i = 1; i < 7; i++){
+                document.getElementById("recent-"+i).innerHTML += `
+                <div class="product-item bg-light mb-4">
+                    <div class="product-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="img/${products[i-1].category}/${products[i-1].obj[i-1].id}.jpg" alt="">
+                        <div class="product-action">
+                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
+                            <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
+                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-sync-alt"></i></a>
+                            <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a>
+                        </div>
+                    </div>
+                    <div class="text-center py-4">
+                        <a class="h6 text-decoration-none text-truncate" href="">${products[i-1].name} ${products[i-1].obj[i-1].id}</a>
+                        <div class="d-flex align-items-center justify-content-center mt-2">
+                            <h5>$${products[i-1].obj[i-1].price}</h5><h6 class="text-muted ml-2"><del>$80</del></h6>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-center mb-1">
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small class="fa fa-star text-primary mr-1"></small>
+                            <small>(99)</small>
+                        </div>
+                    </div>
+                </div>
+                `
+            }
         }
     }
 }
