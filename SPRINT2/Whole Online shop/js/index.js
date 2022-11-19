5810fcd629cc66482e9cac9916360aff792ee8df
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-11-19 10:17:19 +0000

    Loading offers in index.html

--- SPRINT2/Whole Online shop/js/index.js
+++ SPRINT2/Whole Online shop/js/index.js
@@ -84,6 +84,20 @@ function loadData(){
                 </div>
                 `
             }
+
+            //Load Offer
+            for(let i = 1; i < 3; i++){
+                document.getElementById("offerd-"+i).innerHTML += `
+                <div class="product-offer mb-30" style="height: 300px;">
+                    <img class="img-fluid" src="img/offer${i}.png" alt="">
+                    <div class="offer-text">
+                        <h6 class="text-white text-uppercase">Save 15%</h6>
+                        <h3 class="text-white mb-3">Special Offer</h3>
+                        <a href="" class="btn btn-primary">Entra</a>
+                    </div>
+                </div>
+                `
+            }
         }
     }
 }
