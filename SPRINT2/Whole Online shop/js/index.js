6c89fbd95032b2775453040ed53244a1a648ff25
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-11-19 09:05:31 +0000

    adding categories in index.html

--- SPRINT2/Whole Online shop/js/index.js
+++ SPRINT2/Whole Online shop/js/index.js
@@ -5,16 +5,17 @@ function loadData(){
     http.onload = function(){
         if(this.readyState == 4 && this.status == 200){
             let products = JSON.parse(this.responseText);
+            console.log(products[0])
             //Load Categories.
             for (let i = 1; i < 2; i++){
-                document.getElementById('category'+i).innerHTML +=`
+                document.getElementById('category-space'+i).innerHTML +=`
                 <a class="text-decoration-none" href="">
                     <div class="cat-item d-flex align-items-center mb-4">
                         <div class="overflow-hidden" style="width: 100px; height: 100px;">
                             <img class="img-fluid" src="img/cat-1.jpg" alt="">
                         </div>
                         <div class="flex-fill pl-3" id="category1">
-                            <h6>Category Name</h6>
+                            <h6>${products[0].category}</h6>
                             <small class="text-body">100 Products</small>
                         </div>
                     </div>
