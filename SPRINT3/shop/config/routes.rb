e46a9825848ef79b1ed46d037dd5a942bded1993
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-26 19:19:54 +0000

    Setting things to add user

--- SPRINT3/shop/config/routes.rb
+++ SPRINT3/shop/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   #get 'home/index'
   resources :products
   resources :categories
+  resources :session, only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
