d2cd54ee980cc5896eed4a6b652eca70671fe4a1
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-29 21:59:08 +0000

    .

--- SPRINT3/shop2/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'home/index'
-  resources :products
-  resources :categories
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "home#index"
-end
