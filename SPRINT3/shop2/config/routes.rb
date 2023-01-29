4826eafb4971050aa08300999b608261cd77f01e
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-29 22:13:14 +0000

    Finished shop2

--- SPRINT3/shop2/config/routes.rb
+++ SPRINT3/shop2/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  resources :products
+  resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, param: :_username
   # Defines the root path route ("/")
--- SPRINT3/shop2/db/schema.rb
+++ SPRINT3/shop2/db/schema.rb
@@ -10,7 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_185308) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_220341) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.float "discount"
+    t.float "previous_price"
+    t.float "valorations"
+    t.float "number_of_valorations"
+    t.string "available_size"
+    t.string "img_path"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -20,4 +42,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_185308) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "products", "categories"
 end
